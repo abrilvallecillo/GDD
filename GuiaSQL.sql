55d9ad5e52461ab8c143f8946fe90f7380e9feb6
abrilvallecillo <avallecillo@frba.utn.edu.ar>
2024-09-11 15:28:12 -0300

    PRESTAR ATENCION A COMO HACER JOIN CON LAS TABLAS

--- GuiaSQL.sql
+++ GuiaSQL.sql
@@ -1,27 +1,28 @@
+-----------------------------------------------------------1-----------------------------------------------------------
+
 -- Mostrar el código, razón social de todos los clientes cuyo límite de crédito sea mayor o igual a $ 1000 ordenado por código de cliente.
+
 SELECT clie_codigo AS 'Codigo del cliente', 
         clie_razon_social AS 'Razon Social del cliente'
 FROM Cliente
 WHERE clie_limite_credito >= 1000
 ORDER BY clie_codigo;
 
---------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------2-----------------------------------------------------------
 
 -- Mostrar el código, detalle de todos los artículos vendidos en el año 2012 ordenados por cantidad vendida.
 
-/*
-1. Empezamos por el FROM --> Donde estan las ventas de los productos (19484 Renglones de Facturas)
+-- 1. Empezamos por el FROM --> Donde estan las ventas de los productos (19484 Renglones de Facturas)
 
-SELECT * 
-FROM Item_Factura 
+SELECT * FROM Item_Factura 
 
-2. Me fijo que tengo que traer y en que tabla se encuentra (19484 Prodctos, correspondientes a un Renglon de la Factura) 
+-- 2. Me fijo que tengo que traer y en que tabla se encuentra (19484 Prodctos, correspondientes a un Renglon de la Factura) 
 
 SELECT * 
 FROM Item_Factura 
 JOIN Producto ON Item_Factura.item_producto = Producto.prod_codigo 
 
-3. Que es la cantidad vendida?? Contar todas las cantidades de un producto --> Juntas el mismo codigo y luego lo sumas
+-- 3. Que es la cantidad vendida?? Contar todas las cantidades de un producto --> Juntas el mismo codigo y luego lo sumas
 
 SELECT Producto.prod_codigo, Producto.prod_detalle, SUM(Item_Factura.item_cantidad)
 FROM Item_Factura 
@@ -29,7 +30,7 @@ JOIN Producto ON Item_Factura.item_producto = Producto.prod_codigo
 GROUP BY Producto.prod_codigo, Producto.prod_detalle
 ORDER BY SUM(Item_Factura.item_cantidad)
 
-4. Filtro por el año que me interesa
+-- 4. Filtro por el año que me interesa
 
 SELECT Producto.prod_codigo, Producto.prod_detalle, SUM(Item_Factura.item_cantidad)
 FROM Item_Factura 
@@ -39,7 +40,7 @@ WHERE YEAR(Factura.fact_fecha) = 2012
 GROUP BY Producto.prod_codigo, Producto.prod_detalle
 ORDER BY SUM(Item_Factura.item_cantidad)
 
-*/
+-----------------------------------------------------------
 
 SELECT Producto.prod_codigo AS 'Codigo de los Productos', 
         Producto.prod_detalle AS 'Detalle de los Productos',
@@ -51,23 +52,21 @@ WHERE YEAR(Factura.fact_fecha) = 2012
 GROUP BY Producto.prod_codigo, Producto.prod_detalle
 ORDER BY SUM(Item_Factura.item_cantidad)
 
---------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------3-----------------------------------------------------------
 
 -- Realizar una consulta que muestre código de producto, nombre de producto y el stock total, sin importar en que deposito se encuentre, los datos deben ser ordenados por nombre del artículo de menor a mayor.
 
-/*
-1. Empezamos por el FROM --> Donde estan los productos 
+-- 1. Empezamos por el FROM --> Donde estan los productos 
 
-SELECT * 
-FROM Producto
+SELECT * FROM Producto
 
-2. Me fijo que tengo que traer y en que tabla se encuentra
+-- 2. Me fijo que tengo que traer y en que tabla se encuentra
 
 SELECT Producto.prod_codigo, Producto.prod_detalle, STOCK.stoc_cantidad
 FROM Producto 
 JOIN STOCK on Producto.prod_codigo = STOCK.stoc_producto
 
-3. Que es la cantidad vendida?? Contar todas las cantidades de un producto --> Juntas el mismo codigo y luego lo sumas
+-- 3. Que es la cantidad vendida?? Contar todas las cantidades de un producto --> Juntas el mismo codigo y luego lo sumas
 
 SELECT Producto.prod_codigo, Producto.prod_detalle, sum(isnull(stoc_cantidad, 0))
 FROM Producto 
@@ -75,7 +74,7 @@ LEFT JOIN STOCK on Producto.prod_codigo = STOCK.stoc_producto -- Agregar el LEFT
 GROUP BY Producto.prod_codigo, Producto.prod_detalle
 ORDER BY Producto.prod_detalle
 
-*/
+-----------------------------------------------------------
 
 SELECT Producto.prod_codigo AS 'Codigo de los Productos', 
         Producto.prod_detalle AS 'Nombre de los Productos',
@@ -85,24 +84,66 @@ JOIN STOCK ON Producto.prod_codigo = STOCK.stoc_producto
 GROUP BY Producto.prod_codigo, Producto.prod_detalle
 ORDER BY Producto.prod_detalle
 
---------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------4-----------------------------------------------------------
 
 -- Realizar una consulta que muestre para todos los artículos código, detalle y cantidad de artículos que lo componen. 
 -- Mostrar solo aquellos artículos para los cuales el stock promedio por depósito sea mayor a 100.
+
+-- 1. Empezamos por el FROM --> Donde estan los Articulos
+
+SELECT * FROM Producto
+
+SELECT * FROM Composicion
+
+-- 2. Me fijo que tengo que traer y en que tabla se encuentra
+
 SELECT Producto.prod_codigo AS 'Codigo de los Productos', 
         Producto.prod_detalle AS 'Nombre de los Productos',
-        AVG(STOCK.stoc_cantidad) AS 'Cantidad de Producto'
+       COUNT(Composicion.comp_componente) AS 'Cantidad de Productos que lo componene'
 FROM Producto
-LEFT JOIN Composicion ON comp_producto = prod_codigo
-JOIN STOCK ON stoc_producto = prod_codigo
-JOIN DEPOSITO ON stoc_deposito = depo_codigo
+LEFT JOIN Composicion ON Producto.prod_codigo = Composicion.comp_producto
+GROUP BY Producto.prod_codigo, Producto.prod_detalle
+
+-- 3. Filtro por la cantidad de STOCK
+
+SELECT Producto.prod_codigo AS 'Codigo de los Productos', 
+        Producto.prod_detalle AS 'Nombre de los Productos',
+       COUNT( distinct Composicion.comp_componente) AS 'Cantidad de Productos que lo componene'
+FROM Producto
+LEFT JOIN Composicion ON Producto.prod_codigo = Composicion.comp_producto
+JOIN STOCK ON Producto.prod_codigo = STOCK.stoc_producto
 GROUP BY Producto.prod_codigo, Producto.prod_detalle
 HAVING AVG(STOCK.stoc_cantidad) > 100
 
---------------------------------------------------------------------------------------------------------------------------------------
+-------VEAMOS LA ATOMICIDAD... O COMO LA ROMPEMOS-
+
+-- 2196 --> 2184 Productos simples y 6 Productos compuestos (DOS COMPONENTES), es decir que a esos los devolvio dos veces
+SELECT * 
+FROM Producto 
+LEFT JOIN Composicion ON Producto.prod_codigo = Composicion.comp_producto
+
+-- 5612 --> Trae los productos por cada deposito en donde se encuentra
+SELECT * 
+FROM Producto 
+LEFT JOIN Composicion ON Producto.prod_codigo = Composicion.comp_producto
+JOIN STOCK ON Producto.prod_codigo = STOCK.stoc_producto
+
+-----------------------------------------------------------
+
+SELECT Producto.prod_codigo AS 'Codigo de los Productos', 
+        Producto.prod_detalle AS 'Nombre de los Productos',
+       COUNT(Composicion.comp_componente) AS 'Cantidad de Productos que lo componene'
+FROM Producto 
+LEFT JOIN Composicion ON Producto.prod_codigo = Composicion.comp_producto
+GROUP BY Producto.prod_codigo, Producto.prod_detalle
+HAVING Producto.prod_codigo in ( SELECT stoc_producto FROM STOCK GROUP BY stoc_producto HAVING AVG(stoc_cantidad) > 100 )
+                                -- Devuelve todos productos que tienen mas de 100 unidades en stock
+                                
+-----------------------------------------------------------5-----------------------------------------------------------
 
 -- Realizar una consulta que muestre código de artículo, detalle y cantidad de egresos de stock que se realizaron para ese artículo en el año 2012 
 -- (egresan los productos que fueron vendidos). Mostrar solo aquellos que hayan tenido más egresos que en el 2011.
+
 SELECT Producto.prod_codigo AS 'Codigo de los Productos', 
         Producto.prod_detalle AS 'Nombre de los Productos',
         SUM(Item_Factura.item_cantidad) AS 'Cantidad de egresos de STOCK'
@@ -118,7 +159,7 @@ HAVING SUM(Item_Factura.item_cantidad) > ( SELECT SUM(Item_Factura.item_cantidad
 		                        )
 ORDER BY Producto.prod_codigo
 
---------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------6-----------------------------------------------------------
 
 -- Mostrar para todos los rubros de artículos código, detalle, cantidad de artículos de ese rubro y stock total de ese rubro de artículos. 
 -- Solo tener en cuenta aquellos artículos que tengan un stock mayor al del artículo ‘00000000’ en el depósito ‘00’
