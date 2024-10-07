2153d9b1a1e0bc21aafdfc7180e838d56f102706
abrilvallecillo <avallecillo@frba.utn.edu.ar>
2024-10-07 15:58:01 -0300

    7/10

--- Clase-TSQL.sql
+++ Clase-TSQL.sql
@@ -152,4 +152,75 @@ WHILE Boolean_expression
 [BREAK]
     {sql_statement | statement_block}
 [CONTINUE]
----------------------------------------------------IF...ELSE---------------------------------------------------
+---------------------------------------------------CURSOR---------------------------------------------------
+• Declarar el cursor definiendo su contenido
+
+DECLARE cursor_name CURSOR 
+-- Caracterizaciones optativas
+[LOCAL | GLOBAL ] -- Visibilidad 
+[ FORWARD_ONLY | SCROLL] -- Movimiento -- Solo para adelante -- Para adelante y para atras
+[ STATIC | DYNAMIC ] -- Dinamismo -- A medida que lo voy recorriendo no lo cambia -- Se ejecuta al momento de abrirse (Se mantiene siempre actualizado)
+FOR select_statement
+---------------------------------------------------
+• Abrir el cursor
+
+OPEN [ GLOBAL ] cursor_name -- Ejecuta el SELECT
+---------------------------------------------------
+• Recorrido
+
+FETCH [ NEXT | PRIOR | FIRST | LAST]
+FROM [ GLOBAL ] cursor_name
+[INTO @variable_name [....n ]]
+
+@@FETCH_STATUS --> Devuelve el estado de la última instrucción FETCH de cursor ejecutada sobre cualquier cursor que la conexión haya abierto. 
+                --> Esto se usa para leer un cursos 
+
+0 --> La instrucción FETCH se ejecutó correctamente.
+-1 --> La instrucción FETCH ha finalizado con error o la fila estaba más allá del conjunto de resultados.
+-2 --> Falta la fila
+
+@@CURSOR_ROWS --> Devuelve el número de filas correspondientes actualmente al último cursor abierto en la conexión. 
+                --> Para determinar que el número de filas que cumplan las condiciones del cursor se recuperen en el momento en que se vuelve a llamar
+
+-m --> El cursor se llena de forma asincrónica. El valor devuelto (-m) es el número de filas que contiene actualmente el conjunto de claves.
+-1 --> El cursor es dinámico. Como los cursores dinámicos reflejan todos los cambios, el número de filas correspondientes al cursor cambia constantemente. Nunca se puede afirmar que se han recuperado todas las filas que correspondan.
+0 --> No se han abierto cursores, no hay filas calificadas para el último cursor abierto, o éste se ha cerrado o su asignación se ha cancelado.
+n --> El cursor está completamente lleno. El valor obtenido (n) es el número total de filas del cursor.
+---------------------------------------------------
+• Estado 
+
+CURSOR_STATUS (local", 'cursor_name" | 'global", 'cursor _name')
+
+1 --> El conjunto de resultados del cursor tiene al menos una fila
+0 --> El conjunto de resultados del cursor está vacío.
+-1 --> El cursor está cerrado.
+-2 --> No aplicable. (Cuando es variable, osea dinamico)
+-3 --> No existe ningún cursor con el nombre indicado.
+---------------------------------------------------
+• Cerrar el cursor
+
+CLOSE [ GLOBAL ] cursor_name -- No deja disponible esa variable
+---------------------------------------------------
+• Liberar la memoria del cursor
+
+DEALLOCATE [ GLOBAL ] cursor_name -- Borra el contenido -- Libera memoria
+
+---------------------------------------------------EJEMPLO---------------------------------------------------
+Este ejemplo utiliza @@FETCH_STATUS para controlar las actividades del cursor en un bucle WHILE.
+
+DECLARE Employee_Cursor CURSOR 
+FOR SELECT LastName, FirstName FROM Employees
+
+OPEN Employee_Cursor
+
+FETCH NEXT FROM Employee_Cursor
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    FETCH NEXT FROM Employee_Cursor
+END
+
+CLOSE Employee_Cursor
+
+DEALLOCATE Employee_Cursor
+
+*/
