589ba533eab9e5567781b2f67106c3db1b4a58f0
abrilvallecillo <avallecillo@frba.utn.edu.ar>
2024-10-20 11:09:46 -0300

    20-10

--- Guia-TSQL.sql
+++ Guia-TSQL.sql
@@ -8,6 +8,35 @@
 -- 2. Que devuelve la funcion
 -- 3. Cual es la logica de la funcion?? Que debe hacer?? -- Ver el stock de ese producto en el deposito
 
+CREATE FUNCTION estadoDelDeposito(@articulo char(8), @deposito char(2) )
+RETURNS VARCHAR(50)
+AS
+BEGIN 
+    DECLARE @stoc_cantidad NUMERIC(12,2)
+    DECLARE @stock_maximo NUMERIC(12,2)
+    DECLARE @respuesta VARCHAR(50)
+
+    SELECT @stoc_cantidad = STOCK.stoc_cantidad, @stock_maximo = STOCK.stoc_stock_maximo -- Los valores que necesito para saber si esta lleno
+            FROM STOCK 
+            WHERE STOCK.stoc_producto = @articulo and STOCK.stoc_deposito = @deposito
+
+    IF @stock_maximo IS NULL OR @stoc_cantidad >= @stock_maximo
+        SET @respuesta = 'DEPOSITO COMPLETO'
+    ELSE
+        SET @respuesta = 'OCUPACION DEL DEPOSITO '+ @deposito + STR( @stoc_cantidad / @stock_maximo * 100 ) + ' %'
+    RETURN @respuesta
+END 
+GO
+
+---------------------------------------------------
+
+SELECT STOCK.stoc_producto, 
+        STOCK.stoc_deposito, 
+        STOCK.stoc_cantidad, 
+        STOCK.stoc_stock_maximo,
+        DBO.estadoDelDeposito( STOCK.stoc_producto , STOCK.stoc_deposito ) 
+FROM STOCK
+GO
 ---------------------------------------------------2---------------------------------------------------
 
 -- Realizar una función que dado un artículo y una fecha, retorne el stock que existía a esa fecha
@@ -26,7 +55,7 @@ WHERE fact_fecha < '2011-12-16'
 GROUP BY item_producto
 GO
 
----------------------------------------------------
+---------------------------------------------------ABRIL---------------------------------------------------
 
 CREATE FUNCTION stockEn(@articulo CHAR(8), @fecha SMALLDATETIME )
 RETURNS NUMERIC(12,2)
@@ -50,7 +79,13 @@ GO
 
 ---------------------------------------------------
 
-ALTER FUNCTION stockEn2(@articulo CHAR(8), @fecha SMALLDATETIME )
+SELECT Producto.prod_codigo, dbo.stockEn(Producto.prod_codigo, '01/01/2010')
+FROM Producto
+GO
+
+---------------------------------------------------DOCENTE---------------------------------------------------
+
+CREATE FUNCTION stockEn2(@articulo CHAR(8), @fecha SMALLDATETIME )
 RETURNS DECIMAL(12,2)
 AS
 BEGIN 
@@ -59,20 +94,20 @@ BEGIN
         SELECT ISNULL ( SUM ( stoc_cantidad  ) , 0 ) -- Los que tengo
         FROM STOCK
         WHERE stoc_producto = @articulo
+    )
     +
     ( 
         SELECT  ISNULL ( SUM ( item_cantidad ) , 0 )  -- Los que vendi
         FROM Item_Factura
         JOIN Factura ON item_numero = fact_numero AND item_sucursal = fact_sucursal AND Item_tipo = fact_tipo
         WHERE item_producto = @articulo AND fact_fecha <= @fecha 
-    ) 
     )
 END 
 GO
 
 ---------------------------------------------------
 
-SELECT Producto.prod_codigo, dbo.stockEn(Producto.prod_codigo, '01/01/2010')
+SELECT Producto.prod_codigo, dbo.stockEn2(Producto.prod_codigo, '01/01/2010')
 FROM Producto
 GO
 
@@ -90,6 +125,24 @@ GO
 
 -- 2. Necesitamos parametros?? 
     --> Como un porcedimiento no puede retornar valores vamos a tener que crear una variable que simbolice nuestro resultado
+
+CREATE PROCEDURE empleados @vendedor INT OUTPUT
+AS
+BEGIN
+    UPDATE Empleado
+    SET empl_comision = (
+                            SELECT ISNULL ( SUM( fact_total) , 0 )
+                            FROM Factura 
+                            WHERE fact_vendedor = empl_codigo AND YEAR (fact_fecha) = ( SELECT MAX ( YEAR ( fact_fecha ) ) FROM Factura ) 
+                        )
+    SELECT TOP 1 @vendedor = empl_codigo FROM Empleado ORDER BY empl_comision DESC
+    RETURN
+END
+GO
+
+EXEC dbo.comisiones 1
+SELECT * FROM Empleado ORDER BY empl_comision DESC
+GO
 
 ---------------------------------------------------4---------------------------------------------------
 
